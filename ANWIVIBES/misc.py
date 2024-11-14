2d3a0e6b16ab6c12a65c48cfca8d6f32f2a8c9ff
Dvis <gokhaledurgesh4@gmail.com>
2024-11-14 11:47:05 +0530

    Auto Fixes

---
+++ ANWIVIBES/misc.py
@@ -0,0 +1,84 @@
+#
+# Copyright (C) 2024 by IamDvis@Github, < https://github.com/IamDvis >.
+#
+# This file is part of < https://github.com/IamDvis/DV-VIBES > project,
+# and is released under the MIT License.
+# Please see < https://github.com/IamDvis/DV-VIBES/blob/master/LICENSE >
+#
+# All rights reserved.
+
+import socket
+import time
+
+import heroku3
+from pyrogram import filters
+
+import config
+from ANWIVIBES.core.mongo import mongodb
+
+from .logging import LOGGER
+
+SUDOERS = filters.user()
+
+HAPP = None
+_boot_ = time.time()
+
+
+def is_heroku():
+    return "heroku" in socket.getfqdn()
+
+
+XCB = [
+    "/",
+    "@",
+    ".",
+    "com",
+    ":",
+    "git",
+    "heroku",
+    "push",
+    str(config.HEROKU_API_KEY),
+    "https",
+    str(config.HEROKU_APP_NAME),
+    "HEAD",
+    "master",
+]
+
+
+def dbb():
+    global db
+    db = {}
+    LOGGER(__name__).info(f"✦ Local Database Initialized...💛")
+
+
+async def sudo():
+    global SUDOERS
+    SUDOERS.add(config.OWNER_ID)
+    sudoersdb = mongodb.sudoers
+    sudoers = await sudoersdb.find_one({"sudo": "sudo"})
+    sudoers = [] if not sudoers else sudoers["sudoers"]
+    if config.OWNER_ID not in sudoers:
+        sudoers.append(config.OWNER_ID)
+        await sudoersdb.update_one(
+            {"sudo": "sudo"},
+            {"$set": {"sudoers": sudoers}},
+            upsert=True,
+        )
+    if sudoers:
+        for user_id in sudoers:
+            SUDOERS.add(user_id)
+    LOGGER(__name__).info(f"✦ Sudoers Loaded...🧡")
+
+
+def heroku():
+    global HAPP
+    if is_heroku:
+        if config.HEROKU_API_KEY and config.HEROKU_APP_NAME:
+            try:
+                Heroku = heroku3.from_key(config.HEROKU_API_KEY)
+                HAPP = Heroku.app(config.HEROKU_APP_NAME)
+                LOGGER(__name__).info(f"✦ Heroku App Configured...💙")
+            except BaseException:
+                LOGGER(__name__).warning(
+                    f"✦ Please make sure your Heroku API Key and Your App name are configured correctly in the heroku...💚"
+                )
