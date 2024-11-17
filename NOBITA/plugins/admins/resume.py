515f1dcede68ab4752e8c3ba6e0738ea7703f562
tu-mera-dusman <ramuseth41@gmail.com>
2024-11-17 21:01:58 +0530

    Update resume.py

--- NOBITA/plugins/admins/resume.py
+++ NOBITA/plugins/admins/resume.py
@@ -1,20 +1,11 @@
-#
-# Copyright (C) 2024 by IamDvis@Github, < https://github.com/IamDvis >.
-#
-# This file is part of < https://github.com/IamDvis/DV-VIBES > project,
-# and is released under the MIT License.
-# Please see < https://github.com/IamDvis/DV-VIBES/blob/master/LICENSE >
-#
-# All rights reserved.
-
 from pyrogram import filters
 from pyrogram.types import Message
 
-from ANWIVIBES import app
-from ANWIVIBES.core.call import ANWI
-from ANWIVIBES.utils.database import is_music_playing, music_on
-from ANWIVIBES.utils.decorators import AdminRightsCheck
-from ANWIVIBES.utils.inline import close_markup
+from NOBITA import app
+from NOBITA.core.call import NOBITA
+from NOBITA.utils.database import is_music_playing, music_on
+from NOBITA.utils.decorators import AdminRightsCheck
+from NOBITA.utils.inline import close_markup
 from config import BANNED_USERS
 
 
@@ -24,7 +15,7 @@ async def resume_com(cli, message: Message, _, chat_id):
     if await is_music_playing(chat_id):
         return await message.reply_text(_["admin_3"])
     await music_on(chat_id)
-    await ANWI.resume_stream(chat_id)
+    await NOBITA.resume_stream(chat_id)
     await message.reply_text(
         _["admin_4"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
