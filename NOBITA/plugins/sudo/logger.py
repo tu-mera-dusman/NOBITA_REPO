c6199f81cc18bdbbc11782a51e09c9f831dc52e5
tu-mera-dusman <ramuseth41@gmail.com>
2024-11-17 22:12:22 +0530

    Update logger.py

--- NOBITA/plugins/sudo/logger.py
+++ NOBITA/plugins/sudo/logger.py
@@ -1,18 +1,9 @@
-#
-# Copyright (C) 2024 by IamDvis@Github, < https://github.com/IamDvis >.
-#
-# This file is part of < https://github.com/IamDvis/DV-VIBES > project,
-# and is released under the MIT License.
-# Please see < https://github.com/IamDvis/DV-VIBES/blob/master/LICENSE >
-#
-# All rights reserved.
-
 from pyrogram import filters
 
-from ANWIVIBES import app
-from ANWIVIBES.misc import SUDOERS
-from ANWIVIBES.utils.database import add_off, add_on
-from ANWIVIBES.utils.decorators.language import language
+from NOBITA import app
+from NOBITA.misc import SUDOERS
+from NOBITA.utils.database import add_off, add_on
+from NOBITA.utils.decorators.language import language
 
 
 @app.on_message(filters.command(["logger"]) & SUDOERS)
