e514794a7ca921bd53e37c9edbf6ced31bc6f488
tu-mera-dusman <ramuseth41@gmail.com>
2024-11-17 20:10:54 +0530

    Add files via upload

---
+++ NOBITA/tools/clean.py
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2024 by IamDvis@Github, < https://github.com/IamDvis >.
+#
+# This file is part of < https://github.com/IamDvis/DV-VIBES > project,
+# and is released under the MIT License.
+# Please see < https://github.com/IamDvis/DV-VIBES/blob/master/LICENSE >
+#
+# All rights reserved.
+
+import os
+import shutil
+
+from pyrogram import filters
+
+from ANWIVIBES import app
+from ANWIVIBES.misc import SUDOERS
+
+
+@app.on_message(filters.command("clean") & SUDOERS)
+async def clean(_, message):
+    A = await message.reply_text("ᴄʟᴇᴀɴɪɴɢ ᴛᴇᴍᴘ ᴅɪʀᴇᴄᴛᴏʀɪᴇs...")
+    dir = "downloads"
+    dir1 = "cache"
+    shutil.rmtree(dir)
+    shutil.rmtree(dir1)
+    os.mkdir(dir)
+    os.mkdir(dir1)
+    await A.edit("ᴛᴇᴍᴘ ᴅɪʀᴇᴄᴛᴏʀɪᴇs ᴀʀᴇ ᴄʟᴇᴀɴᴇᴅ")
