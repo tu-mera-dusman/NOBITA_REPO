015ade8f4deeac5f531403bdce2a4c0a26a29a04
tu-mera-dusman <ramuseth41@gmail.com>
2024-11-21 09:04:53 +0530

    Update start.py

--- NOBITA/utils/inline/start.py
+++ NOBITA/utils/inline/start.py
@@ -1,16 +1,7 @@
-#
-# Copyright (C) 2024 by IamDvis@Github, < https://github.com/IamDvis >.
-#
-# This file is part of < https://github.com/IamDvis/DV-VIBES > project,
-# and is released under the MIT License.
-# Please see < https://github.com/IamDvis/DV-VIBES/blob/master/LICENSE >
-#
-# All rights reserved.
-
 from pyrogram.types import InlineKeyboardButton
 
 import config
-from ANWIVIBES import app
+from NOBITA import app
 
 
 def start_panel(_):
