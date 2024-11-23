4b2e948ebf9aab9459165efea81996903f725336
tu-mera-dusman <ramuseth41@gmail.com>
2024-11-23 15:46:41 +0530

    Update logging.py

--- NOBITA/logging.py
+++ NOBITA/logging.py
@@ -1,12 +1,3 @@
-#
-# Copyright (C) 2024 by IamDvis@Github, < https://github.com/IamDvis >.
-#
-# This file is part of < https://github.com/IamDvis/DV-VIBES > project,
-# and is released under the MIT License.
-# Please see < https://github.com/IamDvis/DV-VIBES/blob/master/LICENSE >
-#
-# All rights reserved.
-
 import logging
 
 logging.basicConfig(
