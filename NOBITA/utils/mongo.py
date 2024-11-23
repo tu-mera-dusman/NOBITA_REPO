21aa5b9f54bc060bf78a45c74d2bcfd2bd3848f7
tu-mera-dusman <ramuseth41@gmail.com>
2024-11-23 15:06:21 +0530

    Update mongo.py

--- NOBITA/utils/mongo.py
+++ NOBITA/utils/mongo.py
@@ -1,12 +1,3 @@
-#
-# Copyright (C) 2024 by IamDvis@Github, < https://github.com/IamDvis >.
-#
-# This file is part of < https://github.com/IamDvis/DV-VIBES > project,
-# and is released under the MIT License.
-# Please see < https://github.com/IamDvis/DV-VIBES/blob/master/LICENSE >
-#
-# All rights reserved.
-
 from typing import Dict, Union
 
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
@@ -14,4 +5,4 @@ from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 from config import MONGO_DB_URI
 
 mongo = MongoCli(MONGO_DB_URI)
-db = mongo.TanuMusic
+db = mongo.AaruMusic
